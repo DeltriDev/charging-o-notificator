1b6975de4f2408fea4c086a86e6eddd1218fec2c
SamAlexandros <98485800+SamAlexandros@users.noreply.github.com>
2023-03-14 18:53:43 +0200

    changes for different percentages

--- charger-o-metter.py
+++ charger-o-metter.py
@@ -19,17 +19,9 @@ def convertTime(seconds):
     minutes, seconds = divmod(seconds, 60)
     hours, minutes = divmod(minutes, 60)
     return "%d:%02d:%02d" % (hours, minutes, seconds)
-  
-# returns a tuple
-battery = psutil.sensors_battery()
-  
-print("Battery percentage : ", battery.percent)
-print("Power plugged in : ", battery.power_plugged)
-  
-# converting seconds to hh:mm:ss
-print("Battery left : ", convertTime(battery.secsleft))
 
 while(True):
+    battery = psutil.sensors_battery()
     if battery.power_plugged == True:
         notification.notify(
             title = "Your laptop charging review",
@@ -40,14 +32,35 @@ while(True):
             timeout = 10
         )
     else:
-        notification.notify(
-            title = "Your laptop battery review",
-            message = "Battery percentage : {batterypercent}\nScreen time left: {screentime}".format(
-                batterypercent = battery.percent,
-                screentime = convertTime(battery.secsleft)
-            ) ,
-            app_icon = "battery.ico",
-            timeout = 10
-        )
+        if(battery.percent > 50):
+            notification.notify(
+                title = "Your laptop battery review",
+                message = "Battery percentage : {batterypercent}\nScreen time left: {screentime}".format(
+                    batterypercent = battery.percent,
+                    screentime = convertTime(battery.secsleft)
+                ) ,
+                app_icon = "battery.ico",
+                timeout = 10
+            )
+        elif(battery.percent > 20):
+            notification.notify(
+                title = "Your laptop battery review",
+                message = "If possible charge\nBattery percentage : {batterypercent}\nScreen time left: {screentime}".format(
+                    batterypercent = battery.percent,
+                    screentime = convertTime(battery.secsleft)
+                ) ,
+                app_icon = "battery.ico",
+                timeout = 10
+            )
+        else:
+            notification.notify(
+                title = "Charge your laptop!!!",
+                message = "Low battery\nBatter percentage : {batterypercent}\nScreen time left: {screentime}".format(
+                    batterypercent = battery.percent,
+                    screentime = convertTime(battery.secsleft)
+                ) ,
+                app_icon = "battery.ico",
+                timeout = 10
+            )
     print("Now waiting 10 minutes")
     time.sleep(60*10)
